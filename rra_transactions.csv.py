71bcae927169c4924f7eeb52f11479280684ec68
albertmulonga <albertmulonga531@gmail.com>
2026-02-21 11:46:47 +0100

    mon examen

---
+++ rra_transactions.csv.py
@@ -0,0 +1,7 @@
+amount,heure,description,status
+5000,9,Droits de douane Paid,Paid
+12000,10,Taxe sur importation,Paid
+8000,11,Frais de dossier,Non payé
+15000,14,Droits de douane Paid,Paid
+6000,15,Taxe locale,Non payé
+20000,16,Droits sur exportation,Paid
